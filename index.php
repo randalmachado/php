1e3b1db2cde3eb91b63b9dc94d82e789ab5bc2f1
randalmachado <randalmachado@gmail.com>
2018-12-10 10:25:50 -0200

    merda

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-echo"Ola mundo46"
-
-echo"ola randalis"
-=======
-echo"Ola mundo13";
-
-echo"Ola randal";
->>>>>>> 4d9eab4f7e5b64ca26445a7f9542b27c94547b8e
-
+echo"Ola mundo novamente";
 
 ?>
